f3acb71a42b2613536702d8c3d6ef7838e207382
kangduan <duanea0606@gmail.com>
2016-11-16 14:05:57 +0000

    fdkj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+/*<!-- Theme JS-->*/
+<script src="assets/js/theme.js"></script>
+/*<!-- Plugins JS-->*/
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/jquery.stellar.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/classie.js"></script>
+<script src="assets/js/jquery.easing.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/smoothscroll.js"></script>
+<script src="assets/js/jssor.slider.mini.js"></script>
