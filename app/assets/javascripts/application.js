062c58be03d6c19eee786f4a687841ce6e5081cd
kangduan <duanea0606@gmail.com>
2016-11-17 02:16:58 +0000

    fdkj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require classie
-//= require smoothscroll
-//= require theme
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
