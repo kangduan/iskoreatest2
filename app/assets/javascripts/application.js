e86fb4c64ed45acfc1e517cadbd3397b5ff57b90
kangduan <duanea0606@gmail.com>
2016-11-17 07:17:17 +0000

    dfklj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 //= require bootstrap.min
 //= require classie
 //= require isotope.pkgd.min
@@ -27,3 +23,7 @@
 //= require theme
 //= require waypoints.min
 //= require wow.min
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
