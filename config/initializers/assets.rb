325ac34b6a1d7d2cf0e6879331b791e1e93abf60
kangduan <duanea0606@gmail.com>
2016-11-17 04:05:25 +0000

    fdkj

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js, classie.js, isotope.pkgd.min.js, jquery-1.11.1.min.js,
- jquery.counterup.min.js, jquery.easing.min.js, jquery.stellar.min.js, jssor.slider.mini.js, owl.carousel.min.js, smoothscroll.js,
-  theme.js, waypoints.min.js, wow.min.js)
