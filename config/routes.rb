d94f99ab5d41e4d914959ec39459b2046edf9c54
kangduan <duanea0606@gmail.com>
2016-11-22 01:42:50 +0000

    dfkj

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   post 'home/write'
+  get 'home/about'
   get 'home/product'
 
   # Example of regular route:
