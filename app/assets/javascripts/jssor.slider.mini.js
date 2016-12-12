8d60e3d03b23bd26296753cd4e318d2c4a01fe9b
kangduan <duanea0606@google.com>
2016-12-12 07:24:55 +0000

    dfk

--- app/assets/javascripts/jssor.slider.mini.js
+++ app/assets/javascripts/jssor.slider.mini.js
@@ -44,7 +44,23 @@ new(function(){});var e=i.$JssorEasing$={$EaseSwing:function(a){return-c.cos(a*c
         };
 
         var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+        //responsive code begin
+//you can remove responsive code if you don't want the slider scales while window resizes
+function ScaleSlider() {
+    var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+    if (parentWidth)
+        jssor_slider1.$ScaleWidth(Math.min(parentWidth, 600));
+    else
+        window.setTimeout(ScaleSlider, 30);
+}
+ScaleSlider();
 
+$(window).bind("load", ScaleSlider);
+$(window).bind("resize", ScaleSlider);
+$(window).bind("orientationchange", ScaleSlider);
+//responsive code end
+
+/*
         //responsive code begin
         //you can remove responsive code if you don't want the slider scales while window resizing
         function ScaleSlider() {
@@ -62,3 +78,4 @@ new(function(){});var e=i.$JssorEasing$={$EaseSwing:function(a){return-c.cos(a*c
         $(window).bind("orientationchange", ScaleSlider);
         //responsive code end
     });
+*/
