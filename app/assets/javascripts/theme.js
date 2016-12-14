b4b4f3d76dc42efc5a11f128b142f4ab5240376c
kangduan <duanea0606@google.com>
2016-12-14 06:05:41 +0000

    dfkl

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -192,7 +192,7 @@ $('.scrolltotop').click(function(){
 //product nav
 $(function() {
 $(window).scroll(function(){
-    if ($(window).height() < $('.footer-product').height()) {
+    if ($(this).scrollTop() > $('.footer-product').height()) {
         $('.navbar-fixed-bottom').fadeOut();
     } else {
         $('.navbar-fixed-bottom').fadeIn();
