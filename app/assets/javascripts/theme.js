fc37752fbdbdee3b55260f61f8d75868d87d9eed
kangduan <duanea0606@gmail.com>
2016-12-20 09:21:03 +0000

    fdjk

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -194,7 +194,7 @@ $('.scrolltotop').click(function(){
 // Close mobile menu when click menu link (Bootstrap default menu)
 ////////////////////////////////////////////////////////////////////
 $(function() {
-$(document).on('click','.navbar-collapse.in',function(e) {
+$(document).on('click','.navbar-collapse',function(e) {
     if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
         $(this).collapse('hide');
     }
